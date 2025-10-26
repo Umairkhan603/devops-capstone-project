7e21de6053bab246d33f95273e42eb41ad3bd1ce
Umairkhan603 <umairnutkani603@gmail.com>
2025-10-26 03:10:33 -0400

    Add static init_db method to Account class

--- service/models.py
+++ service/models.py
@@ -9,7 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
 
-# Create the SQLAlchemy object to be initialized later in init_db()
+# Create the SQLAlchemy object to be initialized later
 db = SQLAlchemy()
 
 
@@ -34,33 +34,33 @@ class PersistentBase:
         self.id = None  # pylint: disable=invalid-name
 
     def create(self):
-        """Creates an Account in the database."""
-        logger.info("Creating %s", self.name)
+        """Creates a record in the database."""
+        logger.info("Creating record: %s", getattr(self, "name", "unknown"))
         self.id = None  # id must be None to generate a new primary key
         db.session.add(self)
         db.session.commit()
 
     def update(self):
-        """Updates an Account in the database."""
-        logger.info("Updating %s", self.name)
+        """Updates a record in the database."""
+        logger.info("Updating record: %s", getattr(self, "name", "unknown"))
         db.session.commit()
 
     def delete(self):
-        """Removes an Account from the data store."""
-        logger.info("Deleting %s", self.name)
+        """Removes a record from the data store."""
+        logger.info("Deleting record: %s", getattr(self, "name", "unknown"))
         db.session.delete(self)
         db.session.commit()
 
     @classmethod
     def all(cls):
         """Returns all of the records in the database."""
-        logger.info("Processing all records")
+        logger.info("Fetching all records for %s", cls.__name__)
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
         """Finds a record by its ID."""
-        logger.info("Processing lookup for id %s ...", by_id)
+        logger.info("Looking up %s by id=%s", cls.__name__, by_id)
         return cls.query.get(by_id)
 
 
@@ -70,13 +70,15 @@ class PersistentBase:
 class Account(db.Model, PersistentBase):
     """Class that represents an Account."""
 
+    __tablename__ = "account"
+
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
-    email = db.Column(db.String(64))
-    address = db.Column(db.String(256))
-    phone_number = db.Column(db.String(32), nullable=True)  # phone number is optional
-    date_joined = db.Column(db.Date(), nullable=False, default=date.today())
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+    address = db.Column(db.String(256), nullable=False)
+    phone_number = db.Column(db.String(32), nullable=True)  # optional
+    date_joined = db.Column(db.Date(), nullable=False, default=date.today)
 
     def __repr__(self):
         return f"<Account {self.name} id=[{self.id}]>"
@@ -100,20 +102,32 @@ class Account(db.Model, PersistentBase):
             self.address = data["address"]
             self.phone_number = data.get("phone_number")
             date_joined = data.get("date_joined")
+
             if date_joined:
                 self.date_joined = date.fromisoformat(date_joined)
             else:
                 self.date_joined = date.today()
+
         except KeyError as error:
-            raise DataValidationError(f"Invalid Account: missing {error.args[0]}") from error
+            raise DataValidationError(
+                f"Invalid Account: missing {error.args[0]}"
+            ) from error
         except TypeError as error:
             raise DataValidationError(
-                f"Invalid Account: body of request contained bad or no data - {error.args[0]}"
+                f"Invalid Account: bad or no data - {error.args[0]}"
             ) from error
+
         return self
 
     @classmethod
     def find_by_name(cls, name):
         """Returns all Accounts with the given name."""
-        logger.info("Processing name query for %s ...", name)
+        logger.info("Querying for accounts with name=%s", name)
         return cls.query.filter(cls.name == name)
+
+    @staticmethod
+    def init_db(app):
+        """Initializes the database with the given Flask app."""
+        db.init_app(app)
+        with app.app_context():
+            db.create_all()
